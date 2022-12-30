4a2bd6da65f6ee21aa71f5709e6d059422cf41fc
Joe <burtonjae@hotmail.co.uk>
2022-12-30 14:14:25 +0000

    Prevent spectators from being able to teleport others

--- src/main/java/net/earthcomputer/cheatlikedefnot/mixin/TeleportCommandMixin.java
+++ src/main/java/net/earthcomputer/cheatlikedefnot/mixin/TeleportCommandMixin.java
@@ -1,22 +1,47 @@
 package net.earthcomputer.cheatlikedefnot.mixin;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.command.TeleportCommand;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.world.GameMode;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
 
 @Mixin(TeleportCommand.class)
 public class TeleportCommandMixin {
+    @Unique
+    private static final SimpleCommandExceptionType OTHER_ENTITY_EXCEPTION = new SimpleCommandExceptionType(Text.literal("You can't teleport other entities"));
+
     @Inject(method = {"method_13764", "method_13763"}, at = @At("HEAD"), cancellable = true)
     private static void changeRequirement(ServerCommandSource source, CallbackInfoReturnable<Boolean> cir) {
         if (source.getEntity() instanceof ServerPlayerEntity player) {
             if (player.interactionManager.getGameMode() == GameMode.SPECTATOR) {
                 cir.setReturnValue(true);
             }
+        }
+    }
+
+    @Inject(method = "teleport", at = @At("HEAD"))
+    private static void preventSpectatorsTeleportingOthers(ServerCommandSource source, Entity target, ServerWorld world, double x, double y, double z, Set<PlayerPositionLookS2CPacket.Flag> movementFlags, float yaw, float pitch, @Nullable TeleportCommand.LookTarget facingLocation, CallbackInfo ci) throws CommandSyntaxException {
+        if (source.hasPermissionLevel(2)) {
+            return;
+        }
+
+        if (target != source.getEntity()) {
+            throw OTHER_ENTITY_EXCEPTION.create();
         }
     }
 }
