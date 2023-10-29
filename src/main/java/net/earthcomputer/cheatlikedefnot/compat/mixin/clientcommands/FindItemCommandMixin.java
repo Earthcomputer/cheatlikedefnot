0819c43d26c3e405335b947cb0f7285aef89c534
Joe <burtonjae@hotmail.co.uk>
2023-10-29 17:37:06 +0000

    Update to 1.20.2, use op find items task now built-in to clientcommands

--- src/main/java/net/earthcomputer/cheatlikedefnot/compat/mixin/clientcommands/FindItemCommandMixin.java
+++ src/main/java/net/earthcomputer/cheatlikedefnot/compat/mixin/clientcommands/FindItemCommandMixin.java
@@ -1,49 +1,20 @@
 package net.earthcomputer.cheatlikedefnot.compat.mixin.clientcommands;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.logging.LogUtils;
 import net.earthcomputer.cheatlikedefnot.CheatLikeDefnot;
-import net.earthcomputer.cheatlikedefnot.compat.clientcommands.CLDFindItemTask;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.function.Predicate;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(targets = "net.earthcomputer.clientcommands.command.FindItemCommand", remap = false)
 @Pseudo
 public class FindItemCommandMixin {
-    @Inject(method = "findItem", at = @At("HEAD"), cancellable = true)
-    private static void onFindItem(CommandContext<FabricClientCommandSource> ctx, boolean noSearchShulkerBox, boolean keepSearching, Pair<String, Predicate<ItemStack>> item, CallbackInfoReturnable<Integer> cir) {
-        if (!CheatLikeDefnot.isCheatLikeDefnotOnServer()) {
-            return;
+    @ModifyArg(method = "makeFindItemsTask", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;hasPermissionLevel(I)Z", remap = true))
+    private static int modifyFindItemsTaskPermissionLevel(int oldPermissionLevel) {
+        if (CheatLikeDefnot.isCheatLikeDefnotOnServer()) {
+            return 0;
+        } else {
+            return oldPermissionLevel;
         }
-
-        try {
-            Class<?> longTaskClass = Class.forName("net.earthcomputer.clientcommands.task.LongTask");
-            String taskName = (String) Class.forName("net.earthcomputer.clientcommands.task.TaskManager").getMethod("addTask", String.class, longTaskClass).invoke(null, "cfinditem", new CLDFindItemTask(item.getLeft(), item.getRight(), !noSearchShulkerBox, keepSearching));
-
-            if (keepSearching) {
-                ctx.getSource().sendFeedback(Text.translatable("commands.cfinditem.starting.keepSearching", item.getLeft())
-                    .append(" ")
-                    .append((Text) Class.forName("net.earthcomputer.clientcommands.command.ClientCommandHelper").getMethod("getCommandTextComponent", String.class, String.class).invoke(null, "commands.client.cancel", "/ctask stop " + taskName)));
-            } else {
-                ctx.getSource().sendFeedback(Text.translatable("commands.cfinditem.starting", item.getLeft()));
-            }
-        } catch (ReflectiveOperationException e) {
-            LogUtils.getLogger().error("Failed to run cheatlikedefnot cfinditem command", e);
-            ctx.getSource().sendFeedback(Text.translatable("cheatlikedefnot.cfinditem.failed").styled(style -> style.withColor(Formatting.YELLOW)));
-            return;
-        }
-
-        cir.setReturnValue(Command.SINGLE_SUCCESS);
     }
 }
